eb4fc7189db137d38a001934831045e4ac88b588
forrestjoy <47678448+forrestjoy@users.noreply.github.com>
2019-04-19 12:04:28 -0700

    Add files via upload

---
+++ partialobj.js
@@ -0,0 +1,27 @@
+var express =require("express"),
+    app = express(),
+    Db = require('mongodb').Db,
+    MongoClient = require('mongodb').MongoClient,
+    ObjectId = require('mongodb').ObjectID,
+    bodyParser = require('body-parser'),
+    gurl = "mongodb://localhost:27017/";
+app.use(bodyParser.json())
+app.get("/search", (req, res, next) => {
+	console.log('body: ', req.body);
+	var query=req.body;//search by all parameters given in 
+	MongoClient.connect(gurl,{useNewUrlParser: true }, function(err, db) {
+		if (err)
+		    throw err;
+		var dbo = db.db("mockdb");
+		dbo.collection("forms").find(query,{projection:{_id:1,description:1,part_type:1,revision:1}}).toArray(function(err, result) {
+			if (err) throw err;
+			res.json(result);
+			db.close();
+		    });
+	    });
+	
+    });
+var url = "mongodb://localhost:27017/";
+app.listen(3000,() =>{
+	console.log("Server running on port 3000");
+    });
